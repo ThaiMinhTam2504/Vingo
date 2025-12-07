25c8a7ebdcea0e4c3f59ad00c73aaa7e68ea9d7a
PhongThai2504 <thaitam25042003@gmail.com>
2025-12-07 23:51:57 +0700

    thêm folder frontend

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import genToken from "../utils/token.js"
 
-const signUp = async (req, res) => {
+export const signUp = async (req, res) => {
     try {
         const { fullName, email, password, mobile, role } = req.body
         const user = await User.findOne({ email })
@@ -28,12 +29,65 @@ const signUp = async (req, res) => {
             mobile,
             password: hashedPassword
         })
+        const token = await genToken(user._id)
+        res.cookie("token", token, {
+            secure: false,
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true
+        })
 
+        return res.status(201).json({
+            message: "Đăng ký thành công.",
+            user
+        })
 
 
     } catch (err) {
-        return res.status(500).json({
-            message: "Đã xảy ra lỗi."
+        return res.status(500).json({ message: `Lỗi đăng ký người dùng. ${err}` })
+    }
+}
+
+export const signIn = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if (!user) {
+            res.status(400).json({
+                message: "Người dùng không tồn tại."
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Mật khẩu không đúng."
+            })
+        }
+
+        const token = await genToken(user._id)
+        res.cookie("token", token, {
+            secure: false,
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true
         })
+
+        return res.status(200).json({
+            message: "Đăng nhập thành công.",
+            user
+        })
+    } catch (err) {
+        return res.status(500).json({ message: `Lỗi đăng nhập người dùng. ${err}` })
+    }
+}
+export const signOut = async (req, res) => {
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({
+            message: "Đăng xuất thành công."
+        })
+    } catch (err) {
+        return res.status(500).json({ message: `Lỗi đăng xuất người dùng. ${err}` })
     }
 }
